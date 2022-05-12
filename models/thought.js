97a5b09e4fcc5b7242415ad45b62657ebe682c98
Elizabeth <ekkjohnson@gmail.com>
2022-05-12 18:40:05 -0500

    thought and reaction models

---
+++ models/thought.js
@@ -0,0 +1,34 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const thoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [reactionSchema],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true,
+      },
+      id: false,
+    }
+  );
+  
+
+const thought = model("thought", thoughtSchema);
+
+module.exports = thought;
