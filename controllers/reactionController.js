ea29fc2afefa8fd3aa855841324ef87bf40d4771
Elizabeth <ekkjohnson@gmail.com>
2022-05-15 13:44:15 -0500

    route changes

--- controllers/reactionController.js
+++ controllers/reactionController.js
@@ -1,31 +1,31 @@
-const reactionController={
+// const reactionController={
 // add Reaction
-addReaction({ params, body }, res) {
-    Thought.findOneAndUpdate(
-      { _id: params.thoughtId },
-      { $addToSet: { reactions: body } },
-      { new: true }
-    )
-      .then((dbThoughtData) => {
-        if (!dbThoughtData) {
-          res.status(404).json({ message: "No thought with this id" });
-          return;
-        }
-        res.json(dbThoughtData);
-      })
-      .catch((err) => res.json(err));
-  },
+// addReaction({ params, body }, res) {
+//     Thought.findOneAndUpdate(
+//       { _id: params.thoughtId },
+//       { $addToSet: { reactions: body } },
+//       { new: true }
+//     )
+//       .then((dbThoughtData) => {
+//         if (!dbThoughtData) {
+//           res.status(404).json({ message: "No thought with this id" });
+//           return;
+//         }
+//         res.json(dbThoughtData);
+//       })
+//       .catch((err) => res.json(err));
+//   },
 
-  //delete Reaction
-  deleteReaction({ params }, res) {
-    Thought.findOneAndUpdate(
-      { _id: params.thoughtId },
-      { $pull: { reactions: { reactionId: params.reactionId } } },
-      { new: true }
-    )
-      .then((dbThoughtData) => res.json(dbThoughtData))
-      .catch((err) => res.json(err));
-  }
-};
+//   //delete Reaction
+//   deleteReaction({ params }, res) {
+//     Thought.findOneAndUpdate(
+//       { _id: params.thoughtId },
+//       { $pull: { reactions: { reactionId: params.reactionId } } },
+//       { new: true }
+//     )
+//       .then((dbThoughtData) => res.json(dbThoughtData))
+//       .catch((err) => res.json(err));
+//   }
+// };
 
-module.exports=reactionController;
+// module.exports=reactionController;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const userRoutes=require ('./userRoutes');
 const thoughtRoutes=require('./thoughtRoutes');
-const reactionRoutes=require('./reactionRoutes');
+// const reactionRoutes=require('./reactionRoutes');
 
-router.use('/reaction', reactionRoutes);
+// router.use('/reaction', reactionRoutes);
 // Add `/users` to created routes 
 router.use('/user', userRoutes);
 
