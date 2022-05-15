7b459e5392675a43a5460824bbaf4d4f12b95d4f
Elizabeth <ekkjohnson@gmail.com>
2022-05-15 13:01:15 -0500

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const { connect, connection } = require('mongoose')
 
-connect('mongodb://localhost/Social_Network',{
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/Social_Network'
+
+connect(connectionString,{
     useNewUrlParser: true,
     useUnifiedTopology: true,   
 })
