140bc5481f2a3449fdc12a002bc3cde5d6129af1
Elizabeth <ekkjohnson@gmail.com>
2022-05-15 13:42:47 -0500

    user controller changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,100 +1,44 @@
-const { User } = require("../models");
-const { populate } = require("../models/user");
+const { ObjectId } = require("mongoose").Types;
+const { User } = require('../models')
 
-const userController = {
-    //get all users
-    getAllUsers(req, res) {
-      User.find({})
-        .populate({
-          path: "thoughts",
-          select: "-__v",
-        })
-        .select("-__v")
-        .sort({ _id: -1 })
-        .then((dbUserData) => res.json(dbUserData))
-        .catch((err) => {
-          console.log(err);
-          res.status(400).json(err);
-        });
-    },
-
-    getUserById({ params }, res) {
-        User.findOne({ _id: params.id })
-          .populate({
-            path: "thoughts",
-            select: "-__v",
-          })
-          .select("-__v")
-          .then((dbUserData) => {
-            // If no user is found
-            if (!dbUserData) {
-              res.status(404).json({ message: "No user found with this ID" });
-              return;
+const friendCount = async () => 
+    User.aggregate([
+        { "$project": {
+            "Count": { 
+                "$size": { "$ifNull": [ "$friends", [] ] }
             }
-            res.json(dbUserData);
-          })
-          .catch((err) => {
+        }}
+      ])
+
+
+module.exports = {
+  // get all users
+  getUsers(req, res) {
+    User.find()
+        .then(async (users) => {
+            const userObj = {
+                users,
+                // friends: await friendCount(users)
+            }
+            console.log(userObj);
+            return res.json(userObj)
+        })
+        .catch((err) => {
             console.log(err);
-            res.status(400).json(err);
-          });
-      },
-    // create user
-  createUser({ body }, res) {
-    User.create(body)
-      .then((dbUserData) => res.json(dbUserData))
-      .catch((err) => res.status(400).json(err));
+            return res.status(500).json(err)
+        })
   },
-  // update user by id
-  updateUser({ params, body }, res) {
-    User.findOneAndUpdate({ _id: params.id }, body, { new: true })
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          res.status(404).json({ message: "No user found with this ID" });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch((err) => res.status(400).json(err));
-  },
-  // delete user
-  deleteUser({ params }, res) {
-    User.findOneAndDelete({ _id: params.id })
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          res.status(404).json({ message: "no user found with this ID" });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch((err) => res.status(400).json(err));
-  },
-
-  // add friend
-  addFriend({ params }, res) {
-    User.findOneAndUpdate(
-      { _id: params.id },
-      { $addToSet: { friends: params.friendsId } },
-      { new: true }
-    )
-      .then((dbUserData) => res.json(dbUserData))
-      .catch((err) => res.status(400).json(err));
-  },
-
-  removeFriend({ params }, res) {
-    User.findOneAndUpdate(
-      { _id: params.id },
-      { $pull: { friends: params.friendsId } },
-      { new: true }
-    )
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          res.status(404).json({ message: "no user found with this ID" });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch((err) => res.status(400).json(err));
-  },
+  getSingleUser(req, res) {
+      User.findOne({ _id: req.params.userId })
+      .then( async (user) => 
+        !user
+            ? res.status(404).json({ message: 'No user found with that ID'})
+            : res.json({
+                user,
+                friendCount: await friendCount()
+            })
+      )
+  }
 };
 
 module.exports = userController;
