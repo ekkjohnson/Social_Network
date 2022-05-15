c74943edba4a15f206d1d2779f428a1e7546cf87
Elizabeth <ekkjohnson@gmail.com>
2022-05-15 12:34:08 -0500

    server

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 3000;
+
+const server = express();
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(routes);
+
+db.once("open", () => {
+  server.listen(PORT, () => {
+    console.log(`Server and API are running on ${PORT}`);
+  });
+});
