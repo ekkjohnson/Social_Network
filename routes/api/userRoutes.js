0a7b752f1d8b3916694fa5ae5a0dabad0991f912
Elizabeth <ekkjohnson@gmail.com>
2022-05-16 14:34:24 -0500

    route fix

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
 
 // Set requirements 
-const { getUsers, getSingleUser } = require('../../controllers/userController')
+const { getUsers, getSingleUser, addUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend } = require('../../controllers/userController')
 
 router.route('/').get(getUsers)
-router.route('/:userId').get(getSingleUser)
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
   module.exports=router;
