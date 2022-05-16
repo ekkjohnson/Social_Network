572d163825021605ac952ca264fdea154985c0f8
Elizabeth <ekkjohnson@gmail.com>
2022-05-15 21:30:31 -0500

    uncomment

--- models/user.js
+++ models/user.js
@@ -38,9 +38,9 @@ const userSchema = new Schema(
   }
 );
 
-// userSchema.virtual("friendCount").get(function () {
-//     return this.friends.length;
-//   });
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+  });
   
 const user = model("user", userSchema);
 
